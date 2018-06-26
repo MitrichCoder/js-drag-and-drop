d5cab01b20dcac359caa8e61b329189f29e1c111
Dmitriy Sheshkov <mts3656060@yandex.ru>
2018-06-26 08:56:52 +0300

    Create a moving box

--- source/js/setup.js
+++ source/js/setup.js
@@ -1,26 +1,73 @@
 'use stict';
 
-var screen = document.querySelector('.screen');
-
 var cursorPoint = document.querySelector('.cursor-point');
 var cursorPointCoords = document.querySelector('.cursor-point__coords');
 var cursorLineHorizontal = document.querySelector('.cursor-line--horizontal');
 var cursorLineVertical = document.querySelector('.cursor-line--vertical');
 
+var box = document.querySelector('.box');
+
 cursorPointCoords.textContent = 'x: ' + (Math.round(document.body.clientWidth / 2)) + ' y: ' + Math.round((document.body.clientHeight / 2));
 
-screen.onmousemove = function (moveEvt) {
-  moveEvt.preventDefault();
+window.onmousemove = function (evt) {
+  evt.preventDefault();
 
   var cursorCoords = {
-    x: moveEvt.clientX,
-    y: moveEvt.clientY
+    x: evt.clientX,
+    y: evt.clientY
   };
 
   cursorPoint.style.cssText = 'left: ' + cursorCoords.x + 'px; top: ' + cursorCoords.y + 'px;';
-  cursorLineHorizontal.style = 'top:' + moveEvt.clientY + 'px;'
-  cursorLineVertical.style = 'left:' + moveEvt.clientX + 'px;'
-  cursorPointCoords.textContent = 'x: ' + moveEvt.clientX + ' y: ' + moveEvt.clientY;
-
-  // console.log(cursorPoint.offsetTop, cursorPoint.offsetLeft/*, moveEvt.clientX, moveEvt.clientY*/);
+  cursorLineHorizontal.style = 'top:' + cursorCoords.y + 'px;'
+  cursorLineVertical.style = 'left:' + cursorCoords.x + 'px;'
+  cursorPointCoords.textContent = 'x: ' + cursorCoords.x + ' y: ' + cursorCoords.y;
 };
+
+// box drag and drop
+box.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var dragged = false;
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    box.style.top = (startCoords.y) + 'px';
+    box.style.left = (startCoords.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+
+    if (dragged) {
+      var onClickPreventDefault = function (evt) {
+        evt.preventDefault();
+        box.removeEventListener('click', onClickPreventDefault)
+      };
+      box.addEventListener('click', onClickPreventDefault);
+    }
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
